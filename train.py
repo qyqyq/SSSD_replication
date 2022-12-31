1f905f4022f367a884fb706efa5fa2bda1106be6
qyqyq <qyq9792@Gmail.com>
2022-12-31 16:17:11 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,71 @@
+import argparse
+import json
+from util import calc_diffusion_hyperparams
+import os
+
+
+def train(output_directory,
+          ckpt_iter,
+          n_iters,
+          iters_per_ckpt,
+          iters_per_logging,
+          learning_rate,
+          use_model,
+          only_generate_missing,
+          masking,
+          missing_k):
+    # generate experiment (local) path
+    local_path = "T{}_beta0{}_betaT{}".format(diffusion_config["T"],
+                                              diffusion_config["beta_0"],
+                                              diffusion_config["beta_T"])
+
+    # Get shared output_directory ready
+    output_directory = os.path.join(output_directory, local_path)
+    if not os.path.isdir(output_directory):
+        os.makedirs(output_directory)
+        os.chmod(output_directory, 0o775)
+    print("output directory", output_directory, flush=True)
+
+    # map diffusion hyperparameters to gpu
+    for key in diffusion_hyperparams:
+        if key != "T":
+            diffusion_hyperparams[key] = diffusion_hyperparams[key].cuda()
+
+    # predefine model
+    net = SSSDS4Imputer(**model_config).cuda()
+    print_size(net)
+
+    # define optimizer
+    optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', type=str, default='config/SSSDS4.json',
+                        help='JSON file for configuration')
+
+    args = parser.parse_args()
+
+    with open(args.config) as f:
+        data = f.read()
+
+    config = json.loads(data)
+    print(config)
+
+    train_config = config["train_config"]  # training parameters
+
+    global trainset_config
+    trainset_config = config["trainset_config"]  # to load trainset
+
+    global diffusion_config
+    diffusion_config = config["diffusion_config"]  # basic hyperparameters
+
+    global diffusion_hyperparams
+    diffusion_hyperparams = calc_diffusion_hyperparams(
+        **diffusion_config)  # dictionary of all diffusion hyperparameters
+
+    global model_config
+    model_config = config['wavenet_config']
+
+    train(**train_config)
