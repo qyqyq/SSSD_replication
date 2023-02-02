438170ecf0bd4308d779351077204a24c6d19332
qyqyq <qyq9792@Gmail.com>
2023-02-02 22:41:33 +0800

    Add files via upload

--- SSSDS4Imputers.py
+++ SSSDS4Imputers.py
@@ -1,7 +1,118 @@
+import math
 import tensorflow as tf
 from tensorflow import keras
 
-class SSSD4Imputer(keras.layers.layer):
+class Residual_block(keras.layers.Layer):
+    def __init__(self, res_channels, skip_channels,
+                 diffusion_step_embed_dim_out, in_channels,
+                 s4_lmax,
+                 s4_d_state,
+                 s4_dropout,
+                 s4_bidirectional,
+                 s4_layernorm):
+        super(Residual_block, self).__init__()
+        self.res_channels = res_channels
+
+        self.fc_t = keras.layers.Dense(diffusion_step_embed_dim_out, self.res_channels)
+
+        self.S41 = S4Layer(features=2 * self.res_channels,
+                           lmax=s4_lmax,
+                           N=s4_d_state,
+                           dropout=s4_dropout,
+                           bidirectional=s4_bidirectional,
+                           layer_norm=s4_layernorm)
+
+        self.conv_layer = keras.layers.Conv2D(self.res_channels, 2 * self.res_channels, kernel_size=3)
+
+        self.S42 = S4Layer(features=2 * self.res_channels,
+                           lmax=s4_lmax,
+                           N=s4_d_state,
+                           dropout=s4_dropout,
+                           bidirectional=s4_bidirectional,
+                           layer_norm=s4_layernorm)
+
+        self.cond_conv = keras.layers.Conv2D(2 * in_channels, 2 * self.res_channels, kernel_size=1)
+
+        self.res_conv = nn.Conv1d(res_channels, res_channels, kernel_size=1)
+        self.res_conv = nn.utils.weight_norm(self.res_conv)
+        nn.init.kaiming_normal_(self.res_conv.weight)
+
+        self.skip_conv = nn.Conv1d(res_channels, skip_channels, kernel_size=1)
+        self.skip_conv = nn.utils.weight_norm(self.skip_conv)
+        nn.init.kaiming_normal_(self.skip_conv.weight)
+
+    def forward(self, input_data):
+        x, cond, diffusion_step_embed = input_data
+        h = x
+        B, C, L = x.shape
+        assert C == self.res_channels
+
+        part_t = self.fc_t(diffusion_step_embed)
+        part_t = part_t.view([B, self.res_channels, 1])
+        h = h + part_t
+
+        h = self.conv_layer(h)
+        h = self.S41(h.permute(2, 0, 1)).permute(1, 2, 0)
+
+        assert cond is not None
+        cond = self.cond_conv(cond)
+        h += cond
+
+        h = self.S42(h.permute(2, 0, 1)).permute(1, 2, 0)
+
+        out = keras.activations.tanh(h[:, :self.res_channels, :]) * keras.activations.sigmoid(h[:, self.res_channels:, :])
+
+        res = self.res_conv(out)
+        assert x.shape == res.shape
+        skip = self.skip_conv(out)
+
+        return (x + res) * math.sqrt(0.5), skip  # normalize for training stability
+
+class Residual_group(keras.layers.Layer):
+    def __init__(self, res_channels, skip_channels, num_res_layers,
+                 diffusion_step_embed_dim_in,
+                 diffusion_step_embed_dim_mid,
+                 diffusion_step_embed_dim_out,
+                 in_channels,
+                 s4_lmax,
+                 s4_d_state,
+                 s4_dropout,
+                 s4_bidirectional,
+                 s4_layernorm):
+        super(Residual_group, self).__init__()
+        self.num_res_layers = num_res_layers
+        self.diffusion_step_embed_dim_in = diffusion_step_embed_dim_in
+
+        self.fc_t1 = keras.layers.Dense(diffusion_step_embed_dim_in, diffusion_step_embed_dim_mid)
+        self.fc_t2 = keras.layers.Denser(diffusion_step_embed_dim_mid, diffusion_step_embed_dim_out)
+
+        self.residual_blocks = []
+        for n in range(self.num_res_layers):
+            self.residual_blocks.append(Residual_block(res_channels, skip_channels,
+                                                       diffusion_step_embed_dim_out=diffusion_step_embed_dim_out,
+                                                       in_channels=in_channels,
+                                                       s4_lmax=s4_lmax,
+                                                       s4_d_state=s4_d_state,
+                                                       s4_dropout=s4_dropout,
+                                                       s4_bidirectional=s4_bidirectional,
+                                                       s4_layernorm=s4_layernorm))
+
+    def forward(self, input_data):
+        noise, conditional, diffusion_steps = input_data
+
+        diffusion_step_embed = calc_diffusion_step_embedding(diffusion_steps, self.diffusion_step_embed_dim_in)
+        diffusion_step_embed = keras.activations.swish(self.fc_t1(diffusion_step_embed))
+        diffusion_step_embed = keras.activations.swish(self.fc_t2(diffusion_step_embed))
+
+        h = noise
+        skip = 0
+        for n in range(self.num_res_layers):
+            h, skip_n = self.residual_blocks[n]((h, conditional, diffusion_step_embed))
+            skip += skip_n
+
+        return skip * math.sqrt(1.0 / self.num_res_layers)
+
+class SSSD4Imputer(keras.layers.Layer):
     def __init__(self, in_channels, res_channels, skip_channels, out_channels,
                  num_res_layers,
                  diffusion_step_embed_dim_in,
@@ -13,7 +124,7 @@ class SSSD4Imputer(keras.layers.layer):
                  s4_bidirectional,
                  s4_layernorm):
         super(SSSD4Imputer, self).__init__()
-        self.init_conv = nn.Sequential(Conv(in_channels, res_channels, kernel_size=1), nn.ReLU())
+        self.init_conv = keras.layers.Conv2D(in_channels, res_channels, kernel_size=1, activation='relu')
 
         self.residual_layer = Residual_group(res_channels=res_channels,
                                              skip_channels=skip_channels,
@@ -28,9 +139,10 @@ class SSSD4Imputer(keras.layers.layer):
                                              s4_bidirectional=s4_bidirectional,
                                              s4_layernorm=s4_layernorm)
 
-        self.final_conv = nn.Sequential(Conv(skip_channels, skip_channels, kernel_size=1),
-                                        nn.ReLU(),
-                                        ZeroConv1d(skip_channels, out_channels))
+        self.final_conv = keras.Sequential([
+            keras.layers.Conv2D(skip_channels, skip_channels, kernel_size=1, activation='relu'),
+            ZeroConv1d(skip_channels, out_channels)
+        ])
 
     def call(self, input_data):
         noise, conditional, mask, diffusion_steps = input_data
